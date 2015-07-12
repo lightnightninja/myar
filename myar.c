b736992412b1c1dc67b8d0605436f3243359a491
lightnightninja <mywho09@gmail.com>
2015-07-12 13:46:46 -0700

    Small updates
    Changed some formatting things, added a check to see if the archive
    name was an actual path to use when later.

--- myar.c
+++ myar.c
@@ -9,52 +9,68 @@
 #define _POSIX_C_SOURCE 200809L
 #define _BSD_SOURCE
 #define BSIZE 128 // Default buffer size
+#define endl printf("\n")
 
-/* Standard libraries */
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-
-/* System call prototypes! */
+#include <stdbool.h>
 #include <unistd.h>
 #include <signal.h>
-
-/* File information */
 #include <sys/stat.h>
-
-/* File Control */
 #include <fcntl.h>
-
-/* errno, and error constants */
 #include <errno.h>
+#include <ar.h>
 
 /* function declarations */
 void print_usage(); //prints out how to use program
 void q(); // handles -q
 void x(); // handles -x
-void t(); // handles -t
+int isAr(int, char *const *); // handles -t
+void t();
 void v(); // handles -v
 void d(); // handles -d
 void A(); // handles -A
 //void w(); //EXTRA CREDIT
 
+void open_file(){
 
-int main(int argc, const char *argv[]) {
 
 
-    char option = argv[1][1];
+}
 
+void append_file(){}
+
+
+
+/* this is used to give a default name in case of no archive name being specified */
+void createAr(){}
+
+
+
+int main(int argc, char *const argv[]) {
+
+    int current = argv[1][1];
+
+    for (int i = 0; i < argc; i++) {
+        printf("argv[%i]: %s\n", i, argv[i]);
+    }
     /* No arguments passed */
     if (argc == 1 || argc == 2)
         print_usage();
 
     /* As long as there are at least two arguments */
-    else if (argc > 2 && argv[1][0] == '-') {
+    else if (argc > 2 && argv[1][1]) {
+
+        if(isAr(argc, argv) == 1)
+            printf("Using archive: %s\n", argv[2]);
+        else
+            printf("Invalid archive.\n");
 
         /* Controls what happens when the flags are recieved*/
-        switch (option) {
+        switch (current) {
             case 'q':
                 q();
                 break;
@@ -64,7 +80,7 @@ int main(int argc, const char *argv[]) {
                 break;
 
             case 't':
-                t();
+                t(argc, argv);
                 break;
 
             case 'd':
@@ -104,6 +120,17 @@ void x(){
 
 }
 
+int isAr(int argc, char *const argv[]){
+
+    for (int i = 0; i < sizeof(argv[2])/sizeof(char); i++)
+        if(argv[2][i] == '.')
+            if (argv[2][i+1] == 'a')
+                return 1;
+
+    print_usage();
+    return 0;
+
+}
 
 /* -t print a concise table of contents of the archive */
 void t(){
@@ -112,7 +139,10 @@ void t(){
 
 }
 
-
+/* -l option is given, the following information is displayed for each
+ file: file mode, number of links, owner name, group name, number of bytes
+ in the file, abbreviated month, day-of-month file was last modified, hour
+ file last modified, minute file last modified, and the pathname. */
 /* -v iff specified with -t, print a verbose table of contents of the archive */
 void v(){
 
@@ -123,6 +153,7 @@ void v(){
 
 /* -d delete named files from the archive */
 void d(){
+
 
     printf("I recieved a d!\n");
 
@@ -136,26 +167,16 @@ void A(){
 
 }
 
-
 /* prints out the usage of myar */
 void print_usage(){
 
-    printf("usage:\t"
-           "-q: quickly append named files to archive\n"
-           "\t\t\tmyar -q archive file ...\n"
-
-           "\t\t-x: extract named files\n"
-           "\t\t\tmyar -x archive [file ...]\n"
-
-           "\t\t-t: print a concise table of contents of the archive\n"
-           "\t\t-v: iff specified with -t, print a verbose table of contents of the archive\n"
-           "\t\t\tmyar -t [-v] archive [file ...]\n"
-
-           "\t\t-d: delete named files from the archive\n"
-           "\t\t\tmyar -d archive file ...\n"
-
-           "\t\t-A: quickly append all “regular” files in the current directory\n"
-           "\t\t\tmyar -A archive file ...\n"
+    printf("usage: ./myar [-qxtvdA] <archivename>.a <files> ... \n\t\t"
+           "-q: quickly append named files to archive\n\t"
+           "-x: extract named files\n\t\t"
+           "-t: print a concise table of contents of the archive\n\t\t"
+           "-v: iff specified with -t, print a verbose table of contents of the archive\n\t\t"
+           "-d: delete named files from the archive\n\t\t"
+           "-A: quickly append all “regular” files in the current directory\n"
 
            //"        Extra credit: \n"
            //"        -w for a given timeout, add all modified files to the archive.\n"
@@ -164,8 +185,8 @@ void print_usage(){
     
 }
 
-/* ~~~~~~ Notes ~~~~~~ */
 
+/* ~~~~~~ Notes ~~~~~~ */
 /* The archive file must use exactly the standard format defined in /usr/inc1ude/ar.h, and may be tested with archives created with the ar command. The options above are compatible with the options having the same name in the ar command. -A is a new option not available with the usual ar command.
 
  For the -q command myar should create an archive file if it doesn’t exist, using permissions 666. For the other commands myar reports an error if the archive does not exist, or is in the wrong format.
