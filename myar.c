a5d33b00510ff327f52fd486ff397f51f385c43f
lightnightninja <mywho09@gmail.com>
2015-07-13 23:12:09 -0700

    Formatting fixes + testing
    Seeing if what I was working on was legit code…

--- myar.c
+++ myar.c
@@ -8,16 +8,15 @@
 /* Defines */
 #define _POSIX_C_SOURCE 200809L
 #define _BSD_SOURCE
-#define BSIZE 128 // Default buffer size
+#define BSIZE 256 // Default buffer size
 #define EMPTYSPACE 1024
-#define endl printf("\n")
 
 #include <sys/types.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <unistd.h>
 #include <signal.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -33,9 +32,9 @@
 void    print_usage(); //prints out how to use program
 int     append(char *, int, int); // handles -q
 void    extract(); // handles -x
-void    print_table(int);
+void    print_table(int, int);
 void    delete(); // handles -d
-int    append_all(); // handles -A
+int     append_all(char *, int); // handles -A
 int     is_ar(int, char *, char);
 //void w(); //EXTRA CREDIT
 
@@ -82,7 +81,6 @@ int main(int argc, const char *argv[]) {
         print_usage();
         exit(EXIT_FAILURE);
     }
-    printf("testing if I get here\n");
 
     file_pos = arch_name_pos + 1;
 
@@ -109,7 +107,8 @@ int main(int argc, const char *argv[]) {
             break;
 
         case 't':
-            print_table(v);
+            print_table(v, arch_fd);
+            close(arch_fd);
             break;
 
         case 'd':
@@ -118,10 +117,10 @@ int main(int argc, const char *argv[]) {
 
         case 'A':
             if (file_count == 0) {
-                append_all(arch, arch_fd, file_fd);
+                if(append_all(arch, arch_fd) == -1)
+                   fprintf(stderr, "Something is wrong...\n");
                 close(file_fd);
             }
-
             else
                 fprintf(stderr, "Please only enter the archive name, no files.\n");
             break;
@@ -156,12 +155,10 @@ struct ar_hdr *_create_header(int file_fd, char *file_name){
     snprintf(header->ar_gid,  sizeof(header->ar_gid),  "%-6ld",  (long)stats.st_gid);
     snprintf(header->ar_mode, sizeof(header->ar_mode), "%-8o",   stats.st_mode);
     snprintf(header->ar_size, sizeof(header->ar_size), "%-10lld",(long long)stats.st_size);
-    snprintf(header->ar_fmag, sizeof(header->ar_fmag), "%c%c",   0x60, 0xa);
+    snprintf(header->ar_fmag, sizeof(ARFMAG), "%s", ARFMAG);
 
-    printf("Buffer written\n");
     return header;
 }
-
 /* -q quickly append named files to archive */
 int append(char *file_name, int arch_fd, int file_fd){
 
@@ -169,29 +166,38 @@ int append(char *file_name, int arch_fd, int file_fd){
     buffer = _create_header(file_fd, file_name);
     size_t bytes_read;
 
-    /* Ensure we are starting on an even boundry, 4 sounded nice, since it's a typical word on 32bit systems */
+    /* This allows to differentiate the boundaries between files, making searching easy */
+
+    char empty[EMPTYSPACE]; // I just realized this could be done using the offset in lseek...
+    for (int i = 0; i < EMPTYSPACE; i++) {
+        empty[i] = 0;
+    }
+    char newlines[]={10, 10, 10, 10};
+
+    /* Ensure we are starting on an even boundry */
     int evenboundry;
-    write(arch_fd, "\n", 1);
-    evenboundry = lseek(arch_fd, 0, SEEK_END) % 2;
-    if (evenboundry-1 != 0) {
-        write(arch_fd, "\n", evenboundry -1);
-    }
-
-    write(arch_fd, (char *)buffer, sizeof(struct ar_hdr));//places the header in the file.
-    write(arch_fd, "\n", 1);
-    if (evenboundry-1 != 0)
-        write(arch_fd, "\n", evenboundry -1);
-    while ((bytes_read = read(file_fd, buffer, BSIZE)) > 0) {
-        if (write(arch_fd, buffer, bytes_read) != bytes_read){
-
-            write(arch_fd, "\n\n", 2);
-            return 1;
+    char blank[] = {0};
+    evenboundry = lseek(arch_fd, 0, SEEK_END) % 4;
+    if (evenboundry != 0) {
+        for (int i = 4-evenboundry; i > 0; i--) {
+            if(write(arch_fd, blank, 1) == -1)
+                return -1;
         }
-
-
-        write(arch_fd, "\n\n", 2);
     }
+    if(write(arch_fd, empty, EMPTYSPACE) == -1)
+        return -1;
 
+    /* Places the header in the file. */
+    if(write(arch_fd, (char *)buffer, sizeof(struct ar_hdr)) == -1)
+        return -1;
+
+    /* Transfers data from file to archive */
+    while ((bytes_read = read(file_fd, buffer, BSIZE)) > 0) {
+        if (write(arch_fd, buffer, bytes_read) != bytes_read) { //checking to make sure we don't have a write error
+            return -1;
+        }
+    }
+    write(arch_fd, newlines, 4);
     return 1;
 
 }
@@ -204,11 +210,21 @@ void extract(){
 
 }
 
+/* Goes and fetches the header information*/
+off_t go_fetch(off_t offset, int arch_fd){
 
-/* -t print a concise table of contents of the archive */
-void print_table(int verbose){
+    char *buffer = malloc(sizeof(struct ar_hdr));
+    off_t arch_size;
+    size_t bytes_read;
 
-    printf("I recieved a t!\n");
+    if ((arch_size = lseek(arch_fd, 0, SEEK_END)) == -1) //Find archive size
+        return -1;
+    while (bytes_read < arch_size) {
+        ;
+    }
+
+
+    return -1;
 
 }
 /* returns whether or not the table is going to be printed in verbose mode */
@@ -223,10 +239,43 @@ int _verbose(const char **argv){
             if (argv[2][1] == 'v')
                 return 1;
     }
-
+    
     return -1;
+    
+}
 
-}//√
+void _file_perms(){
+
+    /* code used from example at: http://codewiki.wikidot.com/c:system-calls:stat
+     * It uses a 'ternary if' statement to determine if the file has a - or r/w/x
+     * it's simliar to asking the if statement (is x > y?)
+     * x : y is the quivalent of the statement evaluating as true, and the y is false
+     * However, this can lead to bad things if it doesn't evaluate correctly, but
+     * for this purpose, it just shortens the code a bit, rather than a bunch of
+     * if and else statements.
+     */
+
+}
+/* -t print a concise table of contents of the archive */
+void print_table(int verbose, int arch_fd){
+
+    struct  ar_hdr *temp = malloc(sizeof(struct ar_hdr)); //storing the new header in temp, so we can access stuffs.
+    off_t offset;
+
+    lseek(arch_fd, SARMAG, SEEK_SET);
+
+    offset = lseek(arch_fd, BSIZE, SEEK_DATA);
+
+
+    /* go fetch all of the names */
+    while (go_fetch(offset, arch_fd) != -1) {
+        printf("%s\n", temp->ar_name);
+    }
+    printf("I recieved a t!\n");//debug
+    free(temp);
+
+}
+
 
 /* -d delete named files from the archive */
 void delete(){
@@ -245,7 +294,7 @@ int append_all(char *arch_name, int arch_fd){
 
     /* Following code modified from: http://pubs.opengroup.org/onlinepubs/7908799/xsh/readdir.html*/
     while ((file = readdir(cur)) != NULL) {
-        if ((file->d_type == DT_REG) && strcmp(file->d_name, arch_name) != 0) { //checking that we aren't looking at our arch
+        if ((file->d_type == DT_REG) && strcmp(file->d_name, arch_name) != 0 && strcmp(file->d_name, ".DS_Store") != 0 && strcmp(file->d_name, "myar.c") != 0) { //checking that we aren't looking at our arch
             if((file_fd = open(file->d_name, O_RDONLY)) != -1){
                 append(file->d_name, arch_fd, file_fd);
                 close(file_fd);
@@ -262,8 +311,6 @@ int append_all(char *arch_name, int arch_fd){
         return -1;
     }
 
-
-    printf("I recieved an A!\n");
     return 1;
 
 }
@@ -285,7 +332,7 @@ void print_usage(){
            //"            myar -w archive [file ...]\n"
            );
     
-}//√
+}
 
 int _input(int argc, const char *argv[], int arch_name_pos){
 
@@ -308,7 +355,7 @@ int _input(int argc, const char *argv[], int arch_name_pos){
     print_usage();
     return 0;
 
-}//√
+}
 
 int is_ar(int fd, char *arch, char c){
 
